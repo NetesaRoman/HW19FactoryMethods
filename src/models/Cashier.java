3497652b9b7c1f561f988d8e0a11ee7a701d7d7a
NetesaRoman <netesroman8@gmail.com>
2022-11-07 00:35:37 +0200

    SecondCommit

--- src/models/Cashier.java
+++ src/models/Cashier.java
@@ -10,10 +10,15 @@ public class Cashier {
     public Ticket sellTicket(String name, int age, double money){
         if(age >= 18 && money >= 50){
             return new AdultTicket(name);
-        }else if(age < 18 && money > 25){
+        }else if(age < 18 && money >= 25){
             return new ChildTicket(name);
         }
-        return null;
+        return new Ticket(name) {
+            @Override
+            public void printTicket() {
+                System.out.println("NOT ENOUGH MONEY!");
+            }
+        };
     }
 
 }
